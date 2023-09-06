0994589003752e4c31fd5e3df5b043abc5ef891f
Mostafa Imani <imani.mostafa.74@gmail.com>
2023-09-06 12:33:03 +0330

    Initial commit

---
+++ app/src/main/java/com/mostafa/imani/rockpaper/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mostafa.imani.rockpaper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mostafa.imani.rockpaper.ui.theme.RockPaperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RockPaperTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    MyIdea2()
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RockPaperTheme {
+        Greeting("Android")
+    }
+}
